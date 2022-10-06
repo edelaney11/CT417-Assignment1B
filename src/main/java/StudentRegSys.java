e91d322605dd3f872ee229aa9fe607f5797be70a
Ethan <delaneyethan1@gmail.com>
2022-10-06 18:35:17 +0100

    Added another test, and altered addStudentToModule method to accept a string as an argument

--- src/main/java/StudentRegSys.java
+++ src/main/java/StudentRegSys.java
@@ -2,16 +2,16 @@ import org.joda.time.DateTime;
 import java.util.ArrayList;
 
 public class StudentRegSys {
-    private ArrayList<Course> courses;
-    private ArrayList<Module> modules;
+    private final ArrayList<Course> courses;
+    private final ArrayList<Module> modules;
     public StudentRegSys(){
         Lecturer lecturer1 = new Lecturer("Bob", 32, new DateTime(1990, 1, 1, 0, 0), 123456);
         Lecturer lecturer2 = new Lecturer("Alice", 35, new DateTime(1987, 1, 1, 0, 0), 123456);
         Lecturer lecturer3 = new Lecturer("Fearghal", 40, new DateTime(1982, 1, 1, 0, 0), 123456);
         Lecturer lecturer4 = new Lecturer("Brian", 45, new DateTime(1977, 1, 1, 0, 0), 123456);
 
-        ArrayList<Module> compSciModules = new ArrayList<Module>(); //Module array initialised
-        ArrayList<Module> engModules = new ArrayList<Module>();
+        ArrayList<Module> compSciModules = new ArrayList<>(); //Module array initialised
+        ArrayList<Module> engModules = new ArrayList<>();
         Module module1 = new Module("Software Engineering", "CT417", lecturer1); //Initialise module
         Module module2 = new Module("Machine Learning", "CT4101", lecturer2);
         Module module3 = new Module("System on Chip Design", "EE451", lecturer3);
@@ -25,13 +25,13 @@ public class StudentRegSys {
         Course compSci = new Course("CS&IT", compSciModules, new DateTime(2020, 9, 1, 0, 0), new DateTime(2021, 5, 1, 0, 0));
         Course eng = new Course("Engineering", engModules, new DateTime(2020, 9, 1, 0, 0), new DateTime(2021, 5, 1, 0, 0));
 
-        modules = new ArrayList<Module>();
+        modules = new ArrayList<>();
         modules.add(module1);
         modules.add(module2);
         modules.add(module3);
         modules.add(module4);
 
-        courses = new ArrayList<Course>();
+        courses = new ArrayList<>();
         courses.add(eng);
         courses.add(compSci);
     }
@@ -42,14 +42,15 @@ public class StudentRegSys {
                 student.setCourse(course);
                 student.setModules(course.getModules());
                 course.addStudent(student);
-                for(Module m : course.getModules()){
-                    m.addStudent(student);
-                }
             }
         }
     }
 
-    public void addStudentToModule(Student student, Module module){
-        module.addStudent(student);
+    public void addStudentToModule(Student student, String moduleName){
+        for(Module m : modules){
+            if(m.getName().equals(moduleName)){
+                m.addStudent(student);
+            }
+        }
     }
 }
--- src/test/java/registrationTest.java
+++ src/test/java/registrationTest.java
@@ -2,6 +2,7 @@ import org.joda.time.DateTime;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class registrationTest {
     @Test
     public void testRegistration(){
@@ -9,5 +10,18 @@ public class registrationTest {
         Student student1 = new Student("Steve", 20, new DateTime(2002, 1, 1, 0, 0), 123456);
         regSys.registerStudent(student1, "CS&IT");
         Assertions.assertEquals("CS&IT", student1.getCourse().getName());
+    }
+
+    @Test
+    public void testAddStudentToModule(){
+        StudentRegSys regSys = new StudentRegSys();
+        Student student1 = new Student("Steve", 20, new DateTime(2002, 1, 1, 0, 0), 123456);
+        regSys.registerStudent(student1, "CS&IT");
+        regSys.addStudentToModule(student1, "System on Chip Design");
+        for(Module module : student1.getCourse().getModules()){
+            if(module.getName().equals("System on Chip Design")){
+                Assertions.assertTrue(module.getStudents().contains(student1));
+            }
+        }
     }
 }
